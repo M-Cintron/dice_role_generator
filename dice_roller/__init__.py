85c172f619b13513611d672e93691a92895f4940
MichaelCintron <89198540+M-Cintron@users.noreply.github.com>
2021-09-18 09:01:26 -0400

    Modified dice_roller/__init__.py and unit/test_dice_roller.py
    Continued to write tests and code for the roller

--- dice_roller/__init__.py
+++ dice_roller/__init__.py
@@ -3,22 +3,61 @@ from random import randint
 
 class DiceRoller:
     """
-
+    Objects instantiated by this class can roll any number of dice and recall all the rolls the instance has made.
     """
+
     def __init__(self):
         self.records = []
 
-    def roll(self):
-        min_val = 1
-        max_val = 20
+    def roll(self, *args):
+        """
+        Roll any number of dice and return the result
+
+        Note: if no arguments are given to roll(), then it rolls 1, 20 sided die.
+        The arguments should be any number of tuples or lists separated by commas, formatted as:
+        (<number of dice being rolled>, <num sides of dice>)
+        """
+
+        min_val = 0
+        max_val = 0
+
+        # if roll is ran with no parameters, assume we are rolling one d20
+        if len(args) == 0:
+            min_val = 1
+            max_val = 20
+
+        for dice_info in args:
+            if not isinstance(dice_info, (tuple, list)):
+                raise TypeError("The arguments must be either lists or tuples")
+
+            num_dice = dice_info[0]
+            num_sides = dice_info[1]
+            if not isinstance(num_dice, int):
+                raise TypeError("The number of dice must be an integer")
+            if not isinstance(num_sides, int):
+                raise TypeError("The number of sides on the dice must be an integer")
+
+            if num_dice <= 0:
+                raise ValueError("Cannot role zero or negative dice")
+            if num_sides <= 0:
+                raise ValueError("Cannot role dice with zero or negative sides")
+
+            min_val += num_dice
+            max_val += num_dice * num_sides
+
         median_val = (min_val + max_val) / 2
 
         role_result = randint(min_val, max_val)
         result = (role_result, min_val, max_val, median_val)
-
         self.records.append(result)
-
         return result
 
     def history(self):
         return self.records
+
+
+if __name__ == '__main__':
+    roll_instance = DiceRoller()
+    print(roll_instance.roll((1, 20), (2, 10), (1, 100)))
+    print(roll_instance.roll([1, 20], [2, 10], [1, 100]))
+
--- tests/unit/test_dice_roller.py
+++ tests/unit/test_dice_roller.py
@@ -21,19 +21,49 @@ class DiceRollerTests(unittest.TestCase):
         # check that the min, max, and median values are correct
         self.assertEqual(roll_0[1:], (1, 20, 10.5))
 
+    # test <dice_roller_instance.roll((1, 20), (2, 10), (1, 100))> returns (<any int from 4 to 140> 4, 140, 72)
     def test_3_rolls(self):
         roll_1 = dice_roller_instance.roll((1, 20), (2, 10), (1, 100))
         self.assertIn(roll_1[0], range(4, 140+1))
         self.assertEqual(roll_1[1:], (4, 140, 72))
 
-    # test <dice_roller_instance.roll((1, 20), (2, 10), (1, 100))> returns (<any int from 4 to 140> 4, 140, 72)
     # test <dice_roller_instance.roll([1, 20], [2, 10], [1, 100])> returns (<any int from 4 to 140> 4, 140, 72)
+    def test_3_rolls_lists(self):
+        roll_2 = dice_roller_instance.roll([1, 20], [2, 10], [1, 100])
+        self.assertIn(roll_2[0], range(4, 140+1))
+        self.assertEqual(roll_2[1:], (4, 140, 72))
+
     # test <dice_roller_instance.roll((10, 1))> returns (10, 10, 10, 10)
-    # test <dice_roller_instance.roll((0, 5))> returns (0, 0, 0, 0)
-    # test <dice_roller_instance.roll((10, 0))> returns (0, 0, 0, 0)
+    def test_10_dice_1_side(self):
+        roll_3 = dice_roller_instance.roll((10, 1))
+        self.assertEqual(roll_3, (10, 10, 10, 10))
+
+    # test <dice_roller_instance.roll((0, 5))> fails due to rolling 0 dice
+    def test_0_dice(self):
+        with self.assertRaises(ValueError):
+            dice_roller_instance.roll((0, 5))
+
+    # test <dice_roller_instance.roll((10, 0))> fails due to rolling 0 sided dice
+    def test_0_sides(self):
+        with self.assertRaises(ValueError):
+            dice_roller_instance.roll((10, 0))
+
     # test <dice_roller_instance.roll((1, -10))> fails due to negative sided die
+    def test_negative_sides(self):
+        with self.assertRaises(ValueError):
+            dice_roller_instance.roll((1, -10))
+
     # test <dice_roller_instance.roll((-1, 10))> fails due to negative number of dice
+    def test_negative_dice(self):
+        with self.assertRaises(ValueError):
+            dice_roller_instance.roll((-1, 10))
+
     # test <dice_roller_instance.roll({2, 5})> fails due to no input not being a list or tuple
+    def test_set_input(self):
+        with self.assertRaises(TypeError):
+            dice_roller_instance.roll({2, 5})
     # test <dice_roller_instance.roll('egg')> fails due to no input not being a list or tuple
+    # test dice_roller_instance.roll((1.2, 5)) fails due to number of dice not being an int
+    # test dice_roller_instance.roll((3, 1.414)) fails due to the number of sides not being an int
 
     pass
