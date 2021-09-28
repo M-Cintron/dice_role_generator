541fca8a39c8d130990e59202b516cc60e66203e
MichaelCintron <89198540+M-Cintron@users.noreply.github.com>
2021-09-28 10:37:38 -0400

    Created experiment_test_file.py
    I am experimenting on seeing how I can use pytest to view the contents of each file, so I can find places where I typed 'role'

---
+++ tests/experiment_test_file.py
@@ -0,0 +1,25 @@
+import unittest
+import os
+
+
+class ExperimentingTest(unittest.TestCase):
+    def test_experiment_method(self):
+        yourpath = 'D:\Python Scripts\dice_roller'
+        yourpath = os.getcwd()
+
+        for root, dirs, files in os.walk(yourpath, topdown=True):
+
+            dirs[:] = [d for d in dirs if d not in ['.git', '.idea', 'personal_notes', '__pycache__', '.pytest_cache',
+                                                    '.github']]
+
+            for file in files:
+                print(f"Dealing with file {os.path.join(root, file)}")
+                with open(os.path.join(root, file), "r") as f:
+                    for line in f:
+                        print(line)
+        self.assertEqual('egg', 'egg')
+
+if __name__ == "__main__":
+    unittest.main()
+    # for name in dirs:
+    #     print('for name in dirs:', os.path.join(root, name))
