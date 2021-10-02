3b1b9b56dc606702023e211c30437890f2c671f7
MichaelCintron <89198540+M-Cintron@users.noreply.github.com>
2021-10-02 08:00:42 -0400

    modified test_find_roles.py
    seeing if a bit of code written is not needed... again to just make sure

--- tests/typo_hunting/test_find_roles.py
+++ tests/typo_hunting/test_find_roles.py
@@ -9,15 +9,15 @@ class ExperimentingTest(unittest.TestCase):
         # get the relative path of the dice_roller main directory by getting the current working directory, and removing
         # the tests directory part.
         path_of_test_directory = os.getcwd()
-        if not path_of_test_directory.endswith("dice_roller"):
-            path_parts = path_of_test_directory.split('/')
-            print(path_parts)
-            dice_roller_index = path_parts.index('dice_roller')
-            # splicing off the last two sub directories from ..\dice_roller\tests\typo_hunting so we can have the relative
-            # path of the whole directory: ..\dice_roller
-            project_relative_path = '/'.join(path_parts[:dice_roller_index + 1])
-        else:
-            project_relative_path = path_of_test_directory
+        # if not path_of_test_directory.endswith("dice_roller"):
+        #     path_parts = path_of_test_directory.split('/')
+        #     print(path_parts)
+        #     dice_roller_index = path_parts.index('dice_roller')
+        #     # splicing off the last two sub directories from ..\dice_roller\tests\typo_hunting so we can have the relative
+        #     # path of the whole directory: ..\dice_roller
+        #     project_relative_path = '/'.join(path_parts[:dice_roller_index + 1])
+        # else:
+        #     project_relative_path = path_of_test_directory
 
         path_parts = path_of_test_directory.split('/')
         print(path_parts)
