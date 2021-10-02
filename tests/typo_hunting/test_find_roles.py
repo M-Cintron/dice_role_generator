ebc8c1ea158bd5d390fa13012fc0be0dce2f84de
MichaelCintron <89198540+M-Cintron@users.noreply.github.com>
2021-10-02 07:56:07 -0400

    modified test_find_roles.py
    added comments throughout the code

--- tests/typo_hunting/test_find_roles.py
+++ tests/typo_hunting/test_find_roles.py
@@ -20,19 +20,22 @@ class ExperimentingTest(unittest.TestCase):
             project_relative_path = path_of_test_directory
 
         # make a dic of file names, relative to the dice_roller main directory, and the values being the number of
-        # 'roles' found in them
+        # 'roles' found in them.
+        # To add a file to acceptable_roles, include the top two directories are:'dice_roller/dice_roller/'
         acceptable_roles = {"dice_roller/dice_roller/README.md": 1,
-                            "dice_roller/dice_roller/tests/typo_hunting/test_find_roles.py": 13,
+                            "dice_roller/dice_roller/tests/typo_hunting/test_find_roles.py": 14,
                             "dice_roller/dice_roller/dice_roller/__init__.py": 1,
                             "dice_roller/dice_roller/tests/unit/test_dice_roller.py": 1,
                             'dice_roller/dice_roller/tests/integration/test_integration.py': 2}
         found_roles = {}
 
+        # go through every file in the main dice_roller directory, including those in sub directories
         for root, dirs, files in os.walk(project_relative_path, topdown=True):
-            # change the list of directories to walk through to NOT include these specified directories
+            # change the list of directories to walk through to NOT include the named directories
             dirs[:] = [d for d in dirs if d not in ['.git', '.idea', 'personal_notes', '__pycache__', '.pytest_cache',
                                                     '.github']]
 
+            # go through each file individually
             for file in files:
                 with open(os.path.join(root, file), "r") as f:
 
@@ -41,10 +44,10 @@ class ExperimentingTest(unittest.TestCase):
                     file_string = file_string.lower()
                     num_roles = file_string.count('role')
 
+                    # if 'role's are found in a file
                     if num_roles > 0:
-                        # get the relative path of the current file starting from the dice_roller directory
+                        # get the relative path of the current file starting from the main dice_roller directory
                         file_path_parts = root.split('/')
-                        print(path_of_test_directory, project_relative_path, root, file)
                         dice_roller_index = file_path_parts.index('dice_roller')
                         relative_file_path = '/'.join(file_path_parts[dice_roller_index:])
 
