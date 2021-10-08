5f9b0d70fcbb87152eb79b25fd54a029ac46a62c
MichaelCintron <89198540+M-Cintron@users.noreply.github.com>
2021-10-08 09:09:35 -0400

    Started writing tests for advantage and disadvantage rolling
    Plan to copy this code over to the instance roll() tests once the code for the static_roll() is complete

--- tests/unit/test_static_roll.py
+++ tests/unit/test_static_roll.py
@@ -7,28 +7,28 @@ class DiceRollerTests(unittest.TestCase):
 
     # test static_roll() with no arguments rolls 1, 20 sided die
     def test_empty_roll(self):
-        roll_0 = DiceRoller.static_roll()
+        roll = DiceRoller.static_roll()
         # check that the random number is some number between 1 and 20 (inclusive)
-        self.assertIn(roll_0[0], range(1, 20+1))
+        self.assertIn(roll[0], range(1, 20+1))
         # check that the min, max, and median values are correct
-        self.assertEqual(roll_0[1:], (1, 20, 10.5))
+        self.assertEqual(roll[1:], (1, 20, 10.5))
 
     # test static_roll() returns the correct response when rolling 3 different dice
     def test_3_rolls(self):
-        roll_1 = DiceRoller.static_roll((1, 20), (2, 10), (1, 100))
-        self.assertIn(roll_1[0], range(4, 140+1))
-        self.assertEqual(roll_1[1:], (4, 140, 72))
+        roll = DiceRoller.static_roll((1, 20), (2, 10), (1, 100))
+        self.assertIn(roll[0], range(4, 140+1))
+        self.assertEqual(roll[1:], (4, 140, 72))
 
     # test static_roll() returns correct response when rolling dice in lists
     def test_3_rolls_lists(self):
-        roll_2 = DiceRoller.static_roll([1, 20], [2, 10], [1, 100])
-        self.assertIn(roll_2[0], range(4, 140+1))
-        self.assertEqual(roll_2[1:], (4, 140, 72))
+        roll = DiceRoller.static_roll([1, 20], [2, 10], [1, 100])
+        self.assertIn(roll[0], range(4, 140+1))
+        self.assertEqual(roll[1:], (4, 140, 72))
 
     # test test static_roll() returns correct response when rolling 10, 1 sided dice
     def test_10_dice_1_side(self):
-        roll_3 = DiceRoller.static_roll((10, 1))
-        self.assertEqual(roll_3, (10, 10, 10, 10))
+        roll = DiceRoller.static_roll((10, 1))
+        self.assertEqual(roll, (10, 10, 10, 10))
 
     # test static_roll() returns correct error when given 0 dice
     def test_0_dice(self):
@@ -85,3 +85,55 @@ class DiceRollerTests(unittest.TestCase):
         with self.assertRaisesRegex(TypeError,
                                     "dice_roller expected tuple/list containing 2 items, got 1"):
             DiceRoller.static_roll([2])
+
+    def test_advantage_works(self):
+        roll = DiceRoller.static_roll(advantage=True, show_advantage_val=True)
+        self.assertIn(roll[0], range(1, 20+1))
+        self.assertEqual(roll[1:4], (1, 20, 10.5))
+        self.assertGreaterEqual(roll[0], roll[4])
+
+    def test_advantage_1_die(self):
+        roll = DiceRoller.static_roll((1, 10), advantage=True, show_advantage_val=True)
+        self.assertIn(roll[0], range(1, 10 + 1))
+        self.assertEqual(roll[1:4], (1, 10, 5.5))
+        self.assertGreaterEqual(roll[0], roll[4])
+
+    def test_advantage_3_dice(self):
+        roll = DiceRoller.static_roll((1, 10), (3, 4), (2, 6), advantage=True, show_advantage_val=True)
+        self.assertIn(roll[0], range(6, 34 + 1))
+        self.assertEqual(roll[1:4], (6, 34, 20.0))
+        self.assertGreaterEqual(roll[0], roll[4])
+
+    def test_disadvantage_works(self):
+        roll = DiceRoller.static_roll(advantage=False, show_advantage_val=True)
+        self.assertIn(roll[0], range(1, 20+1))
+        self.assertEqual(roll[1:4], (1, 20, 10.5))
+        self.assertLessEqual(roll[0], roll[4])
+
+    def test_disadvantage_1_die(self):
+        roll = DiceRoller.static_roll((1, 6), advantage=False, show_advantage_val=True)
+        self.assertIn(roll[0], range(1, 6 + 1))
+        self.assertEqual(roll[1:4], (1, 6, 3.5))
+        self.assertLessEqual(roll[0], roll[4])
+
+    def test_disadvantage_3_dice(self):
+        roll = DiceRoller.static_roll((2, 4), (1, 12), (1, 2), advantage=False, show_advantage_val=True)
+        self.assertIn(roll[0], range(4, 22 + 1))
+        self.assertEqual(roll[1:4], (4, 22, 13.0))
+        self.assertLessEqual(roll[0], roll[4])
+
+    def test_advantage_None(self):
+        roll = DiceRoller.static_roll(advantage=None, show_advantage_val=True)
+        self.assertIn(roll[0], range(1, 20+1))
+        self.assertEqual(roll[1:], (1, 20, 10.5))
+
+    def test_str_advantage_input(self):
+        with self.assertRaisesRegex(TypeError, "the advantage argument must be either True, False or None"):
+            DiceRoller.static_roll(advantage='True')
+
+    def test_int_advantage_input(self):
+        with self.assertRaisesRegex(TypeError, "the advantage argument must be either True, False or None"):
+            DiceRoller.static_roll(advantage=1)
+
+# NOTE: when advantage = True or False and show_advantage_val = True, then static_roll() and roll() return an additional
+# number: the thrown out number; this is added to the end of the tuple
