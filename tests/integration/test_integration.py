53ddd44e1441efde9e78f65e9893dc0e05b93217
MichaelCintron <89198540+M-Cintron@users.noreply.github.com>
2021-10-12 09:01:19 -0400

    Started revising according to pylint
    This file still needs pylint revision

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -5,13 +5,17 @@ import dice_roller
 
 class DiceRollerIntegrationTests(unittest.TestCase):
 
-    # test that .history() returns {} when no rolls have been made
     def test_no_history(self):
+        """
+        test that .history() returns {} when no rolls have been made
+        """
         dice_roller_instance = dice_roller.DiceRoller()
         self.assertEqual(dice_roller_instance.history(), {})
 
-    # test .history() returns the correct output after running .roll()
     def test_history(self):
+        """
+        test .history() returns the correct output after running .roll()
+        """
         dice_roller_instance = dice_roller.DiceRoller()
 
         # run 4 rolls, save the results so we can get the roll result for the assert below
@@ -20,25 +24,28 @@ class DiceRollerIntegrationTests(unittest.TestCase):
         result_2 = dice_roller_instance.roll((1, 20), (2, 10), (1, 100))
         result_3 = dice_roller_instance.roll((10, 1))
 
-        self.assertEqual(dice_roller_instance.history(),
-                         {
-                             'roll_0': {'dice': {'dice_0': {'number_of_dice': 1, 'number_of_sides': 20}},
-                                        'result': result_0[0], 'min': 1, 'max': 20, 'median': 10.5},
-                             'roll_1': {'dice': {'dice_0': {'number_of_dice': 1, 'number_of_sides': 20},
-                                                 'dice_1': {'number_of_dice': 2, 'number_of_sides': 10},
-                                                 'dice_2': {'number_of_dice': 1, 'number_of_sides': 100}},
-                                        'result': result_1[0], 'min': 4, 'max': 140, 'median': 72.0},
-                             'roll_2': {'dice': {'dice_0': {'number_of_dice': 1, 'number_of_sides': 20},
-                                                 'dice_1': {'number_of_dice': 2, 'number_of_sides': 10},
-                                                 'dice_2': {'number_of_dice': 1, 'number_of_sides': 100}},
-                                        'result': result_2[0], 'min': 4, 'max': 140, 'median': 72.0},
-                             'roll_3': {'dice': {'dice_0': {'number_of_dice': 10, 'number_of_sides': 1}},
-                                        'result': result_3[0], 'min': 10, 'max': 10, 'median': 10.0}
-                         }
-                         )
+        self.assertEqual(
+            dice_roller_instance.history(),
+            {
+                'roll_0': {'dice': {'dice_0': {'number_of_dice': 1, 'number_of_sides': 20}},
+                           'result': result_0[0], 'min': 1, 'max': 20, 'median': 10.5},
+                'roll_1': {'dice': {'dice_0': {'number_of_dice': 1, 'number_of_sides': 20},
+                                    'dice_1': {'number_of_dice': 2, 'number_of_sides': 10},
+                                    'dice_2': {'number_of_dice': 1, 'number_of_sides': 100}},
+                           'result': result_1[0], 'min': 4, 'max': 140, 'median': 72.0},
+                'roll_2': {'dice': {'dice_0': {'number_of_dice': 1, 'number_of_sides': 20},
+                                    'dice_1': {'number_of_dice': 2, 'number_of_sides': 10},
+                                    'dice_2': {'number_of_dice': 1, 'number_of_sides': 100}},
+                           'result': result_2[0], 'min': 4, 'max': 140, 'median': 72.0},
+                'roll_3': {'dice': {'dice_0': {'number_of_dice': 10, 'number_of_sides': 1}},
+                           'result': result_3[0], 'min': 10, 'max': 10, 'median': 10.0}
+            }
+        )
 
-    # test that .history() is not messed up by invalid rolls
     def test_history_with_invalid_inputs(self):
+        """
+        test that .history() is not messed up by invalid rolls
+        """
         dice_roller_instance = dice_roller.DiceRoller()
         result_0 = dice_roller_instance.roll((1, 15))
         result_1 = dice_roller_instance.roll((1, 30), (2, 10), (1, 100))
@@ -72,8 +79,11 @@ class DiceRollerIntegrationTests(unittest.TestCase):
                          }
                          )
 
-    # test that .clear() empties the instance's record and that .history() returns {}
     def test_clear_history(self):
+        """
+        test that .clear() empties the instance's record and that .history() returns {}
+        """
+
         dice_roller_instance = dice_roller.DiceRoller()
         dice_roller_instance.roll((1, 200))
         dice_roller_instance.roll((2, 20), (2, 13), (1, 100))
